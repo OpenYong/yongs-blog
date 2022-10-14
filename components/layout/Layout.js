31da27f9ee8d244ba0a8d46211ac8f21f1491a1c
OpenYong <fkszm3@gmail.com>
2022-10-14 17:58:34 +0900

    Add main layout style

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -2,12 +2,14 @@ import React from "react";
 import NavBar from "./NavBar";
 import Header from "./Header";
 
+import classes from "./Layout.module.css";
+
 const Layout = ({ children }) => {
   return (
     <>
       <Header />
       {/* <NavBar /> */}
-      <main>{children}</main>
+      <main className={classes.main}>{children}</main>
     </>
   );
 };
