7ddf231347cca6f0bd5140e3334040f2074c563d
OpenYong <fkszm3@gmail.com>
2022-10-20 18:22:43 +0900

    Create AboutMe Component (홈페이지에 넣을지 고민중)

---
+++ components/hompage/AboutMe.js
@@ -0,0 +1,16 @@
+const AboutMe = () => {
+  return (
+    <div className="px-4">
+      <h1 className="text-2xl font-bold my-4">소개</h1>
+      <p className="text-md">
+        웹개발에 자부심이 있는 개발자 <b>Yong Lee</b> 입니다. 현재{" "}
+        <b>풀스택 개발자</b>로 일하고 있습니다. 프론트엔드와 백엔드 모두
+        좋아하지만, 프론트엔드 개발에 애착을 갖고 있습니다. 새로운 것을 배우는데
+        두려움이 없으며 도전하는 즐거움을 즐기고 있습니다. 저와 함께 여정을 할
+        분들을 찾고 있습니다. 또한 언제든 여러분들의 연락을 기다리고 있겠습니다!
+      </p>
+    </div>
+  );
+};
+
+export default AboutMe;
