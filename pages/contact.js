5926ffa98662b6f5c2117ee67863e8a1d6ad7b91
OpenYong <fkszm3@gmail.com>
2022-10-24 14:10:28 +0900

    연락처 업데이트 예정 추가

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,10 @@
 import React from "react";
+import PageHeader from "../components/ui/PageHeader";
 
 const contact = () => {
-  return <div>contact</div>;
+  return <div>
+    <PageHeader>업데이트 예정🔧</PageHeader>
+  </div>;
 };
 
 export default contact;
