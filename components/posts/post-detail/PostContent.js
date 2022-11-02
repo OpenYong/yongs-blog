ec959fec00d3065d3a0b1d58871c4718b7270534
OpenYong <fkszm3@gmail.com>
2022-11-02 13:41:18 +0900

    Update PostDetailPage

--- components/posts/post-detail/PostContent.js
+++ components/posts/post-detail/PostContent.js
@@ -4,15 +4,17 @@ const PostContent = (props) => {
   const { post, source } = props;
 
   return (
-    <>
-      <h1 className="my-12 mx-4 text-4xl font-bold">{post.title}</h1>
-      <article className="prose bg-white px-4 lg:prose-lg">
+    <div className="">
+      <h1 className="my-12 mx-4 text-4xl font-bold md:mx-0 md:text-center">
+        {post.title}
+      </h1>
+      <article className="prose mx-auto bg-white px-4 lg:prose-lg ">
         <time className="italic text-gray-500">{post.date}</time>
         <div className="">
           <MDXRemote {...source} />
         </div>
       </article>
-    </>
+    </div>
   );
 };
 
