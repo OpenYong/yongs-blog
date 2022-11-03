0443a06eaf5dc6a4dc6cf5d9b0d4c53c05af4f0e
Yong <fkszm3@gmail.com>
2022-11-03 23:54:47 +0900

    Remove useless code

--- pages/projects.js
+++ pages/projects.js
@@ -5,16 +5,6 @@ import AllProjects from "../components/projects/AllProjects";
 const ProjectPage = () => {
   const { data: projectsData, error } = useSWR("/api/projects");
 
-  if (error) return <div>failed to load</div>;
-  if (!projectsData)
-    return (
-      <div>
-        <PageHeader>프로젝트</PageHeader>
-        <p className="my-8 text-center">프로젝트 결과물, 공부📚</p>
-        로딩...
-      </div>
-    );
-
   let isLoading = true;
   if (projectsData) {
     isLoading = false;
