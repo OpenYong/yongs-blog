d2bda52453a4aea973517f87c64b629b6d90989c
OpenYong <fkszm3@gmail.com>
2022-11-09 12:53:19 +0900

    Add animation on project skeleton

--- components/ui/ProjectItemSkeleton.js
+++ components/ui/ProjectItemSkeleton.js
@@ -2,7 +2,7 @@ const ProjectItemSkeleton = () => {
   let list = [];
   for (let index = 0; index < 3; index++) {
     list.push(
-      <div className="group visible flex h-64 flex-col justify-between rounded-md border-0 bg-neutral-400/5 p-4 shadow-lg shadow-neutral-200/50 transition ">
+      <div className="group visible flex h-64 animate-pulse flex-col justify-between rounded-md border-0 bg-neutral-400/5 p-4 shadow-lg shadow-neutral-200/50 transition ">
         <div className="space-y-3">
           <div className="h-6 w-32 rounded-md bg-neutral-400/20"></div>
           <p className="h-12 w-full rounded-md bg-neutral-400/10"></p>
