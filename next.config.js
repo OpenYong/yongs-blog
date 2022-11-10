f414cdb9abab905de5f20b40f43ce4b0ae49fd9a
Yong <fkszm3@gmail.com>
2022-11-10 22:08:04 +0900

    환경 변수 설정 업데이트

--- next.config.js
+++ next.config.js
@@ -1,33 +1,6 @@
 /** @type {import('next').NextConfig} */
-
-const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
-
-module.exports = (phase, { defaultConfig }) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      reactStrictMode: true,
-      swcMinify: true,
-      env: {
-        mongodb_username: "yong",
-        mongodb_password: "4LGndmuLkvGybUlk",
-        mongodb_clustername: "cluster0",
-        mongodb_database: "YongsBlog-dev",
-        github_token:
-          "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",
-      },
-    };
-  }
-
-  return {
-    reactStrictMode: true,
-    swcMinify: true,
-    env: {
-      mongodb_username: "yong",
-      mongodb_password: "4LGndmuLkvGybUlk",
-      mongodb_clustername: "cluster0",
-      mongodb_database: "YongsBlog",
-      github_token:
-        "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",
-    },
-  };
+const nextConfig = {
+  reactStrictMode: true,
 };
+
+module.exports = nextConfig;
